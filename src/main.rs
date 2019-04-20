c05cd6434060fc49162a2ceaafa8be95075a2d26
Rafał Opiłowski <rafalopilowski1@outlook.com>
2019-04-20 15:24:36 +0200

    BUG + FIX: Code passes Rust linter - fails at execution

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ extern crate tokio;
 use std::env;
 use std::io::BufReader;
 use std::ops::Add;
+use std::sync::Arc;
 
 use reqwest::r#async::*;
-use tendril::fmt::Slice;
 use tendril::stream::TendrilSink;
 use tokio::prelude::future::Either;
 use tokio::prelude::*;
@@ -33,73 +33,73 @@ struct Modfile {
     fileID: serde_json::Number,
 }
 
-struct MetaData {
-    filename: String,
-    md5: String,
-}
-
 fn main() {
     let args: Vec<_> = env::args().collect();
     let path: &str = &args[1];
     let file = std::fs::File::open(path).expect("opening manifest.json failed!");
     println!("Manifest opened!");
     let buf_reader = BufReader::new(file);
-    let client = Client::new();
+    let client = Arc::new(Client::new());
     let mod_pack: Modpack = serde_json::from_reader(buf_reader).unwrap();
     println!("Gathered data. Downloading modfiles...");
     tokio::run(futures::lazy(move || {
         for mod_file in mod_pack.files {
-            tokio::spawn(
-                gather_metadata(mod_file, &client).and_then(|(name_dom, md5, url)| {
+            let client_clone = client.clone();
+            tokio::spawn(gather_metadata(mod_file, client_clone.clone()).and_then(
+                move |(name_dom, md5, url)| {
                     check_integrity(name_dom, md5).and_then(
-                        |(file_option, file_path_option): (
-                            Option<tokio::fs::File>,
-                            Option<String>,
-                        )| {
+                        move |(file_option, file_path_option)| {
                             if file_option.is_some() {
-                                let mut file = file_option.unwrap();
+                                let file = file_option.unwrap();
                                 let file_path = file_path_option.unwrap();
-                                download_mod_file(url, &client)
-                                    .and_then(|mut chunk| save_file(file, chunk))
+
+                                Either::A(
+                                    download_mod_file(url, client_clone.clone())
+                                        .and_then(move |chunk| save_file(file, chunk))
+                                        .and_then(move |_| done_print(file_path))
+                                        .and_then(|_| Ok(())),
+                                )
                             } else {
-                                already().and_then(|_| Ok(()))
+                                Either::B(already().and_then(|_| Ok(())))
                             }
-                            //Ok(()).into_future()
                         },
                     )
-                }),
-            );
+                },
+            ));
         }
         Ok(())
     }));
 }
 
-fn save_file(mut file: tokio::fs::File, mut chunk: Chunk) -> impl Future<Item = (), Error = ()> {
-    file.write_buf(&mut chunk)
-        .and_then(|_| Ok(()))
+fn save_file(mut file: tokio::fs::File, chunk: Chunk) -> impl Future<Item = (), Error = ()> {
+    file.write_all(&chunk)
+        .into_future()
         .map_err(|e| eprintln!("{:?}", e))
 }
 
 fn done_print(file_path: String) -> impl Future<Item = (), Error = ()> {
-    Ok(println!("{} <- Done!", file_path)).into_future()
+    println!("{} <- Done!", file_path);
+    Ok(()).into_future()
 }
 
 fn already() -> impl Future<Item = (), Error = ()> {
-    Ok(println!("Already downloaded!")).into_future()
+    println!("Already downloaded!");
+    Ok(()).into_future()
 }
 
 fn gather_metadata(
     mod_file: Modfile,
-    client: &Client,
+    client: Arc<Client>,
 ) -> impl Future<Item = (String, String, String), Error = ()> {
     let url = format!(
         "https://minecraft.curseforge.com/projects/{0}/files/{1}",
         mod_file.projectID, mod_file.fileID
     );
+
     client
         .get(&url)
         .send()
-        .and_then(|response| {
+        .and_then(move |response| {
             response.into_body().from_err().concat2().and_then(|body| {
                 let bytes = body.to_tendril();
                 let html = kuchiki::parse_html().from_utf8().one(bytes);
@@ -122,10 +122,10 @@ fn check_integrity(
         .read(true)
         .write(true)
         .append(true)
-        .open(format!("./mods/{}", &name_dom))
-        .and_then(|mut file| {
+        .open(format!("./mods/{}", name_dom))
+        .and_then(move |mut file| {
             let mut file_bytes: Vec<u8> = vec![];
-            file.read_buf(&mut file_bytes).and_then(|_| {
+            file.read_buf(&mut file_bytes).and_then(move |_| {
                 if md5.as_bytes() == file_bytes.as_slice() {
                     Ok((None, None))
                 } else {
@@ -136,12 +136,12 @@ fn check_integrity(
         .map_err(|e| eprintln!("{:?}", e))
 }
 
-fn download_mod_file(url: String, client: &Client) -> impl Future<Item = Chunk, Error = ()> {
+fn download_mod_file(url: String, client: Arc<Client>) -> impl Future<Item = Chunk, Error = ()> {
     let url_to_download = url.add("/download");
     client
         .get(&url_to_download)
         .send()
-        .and_then(|response| {
+        .and_then(move |response| {
             response
                 .into_body()
                 .from_err()
