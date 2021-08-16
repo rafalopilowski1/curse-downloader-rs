f5dd5fde8c34aab2b168ba211a6c8a539c004910
Rafał Opiłowski <rafalopilowski1@outlook.com>
2021-08-16 22:25:43 +0200

    CLEAN cargo + .gitignore and handle Error at Sink

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 use futures::{SinkExt, StreamExt, TryStreamExt};
 use reqwest::Client;
 use serde::Deserialize;
-use std::{
-    borrow::Cow,
-    env,
-    io::{ErrorKind, Sink},
-    pin::Pin,
-    sync::Arc,
-};
+use std::{env, io::ErrorKind};
 use tokio::io::*;
-use tokio_util::*;
 
 #[derive(Deserialize)]
 struct Modpack {
@@ -42,8 +35,13 @@ async fn main() {
         .for_each_concurrent(None, |mod_file| {
             let client = client.clone();
             async move {
+                let mod_to_save = create_file(format!(
+                    "./mods/{0}-{1}.jar",
+                    &mod_file.projectID, &mod_file.fileID
+                ))
+                .await
+                .expect("creating file error");
                 let response = get_mod_file_response(&mod_file, &client).await;
-                let mod_to_save = create_file("mod.jar").await.expect("creating file error");
                 save_mod_to_file(response, mod_to_save).await
             }
         })
@@ -57,7 +55,7 @@ async fn save_mod_to_file(response: reqwest::Response, mod_to_save: tokio::fs::F
     let buf_writer = tokio::io::BufWriter::new(mod_to_save);
     let mut sink =
         tokio_util::codec::FramedWrite::new(buf_writer, tokio_util::codec::BytesCodec::new());
-    sink.send_all(&mut file_stream).await;
+    sink.send_all(&mut file_stream).await.expect("saving error");
 }
 
 async fn get_mod_file_response(mod_file: &Modfile, client: &Client) -> reqwest::Response {
